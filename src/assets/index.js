6f507ffab4e586e3fa33f6199dd3cffa31bd6193
Stefani Dimitrova <dimitrovastefani00@gmail.com>
2023-07-10 12:54:06 +0100

    Navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,6 +30,8 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import sd_logo from './sd_logo.svg';
+
 export {
   logo,
   backend,
@@ -59,4 +61,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  sd_logo,
 };
