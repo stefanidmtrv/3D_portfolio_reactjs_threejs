848320822ff7b40abf838e21cd8c4432b2d31f91
Stefani Dimitrova <dimitrovastefani00@gmail.com>
2023-07-11 16:08:18 +0100

    Finished overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import {motion} from 'framer-motion';
 import { styles } from '../style';
 import { services } from '../constants';
 import {fadeIn, textVariant } from '../utils/motion';
-
+import { SectionWrapper } from '../hoc';
 const ServiceCard =({ index, title, icon}) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -52,4 +52,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About , "about")
